48b904ecfe33625a3116907d3033edc087cff160
rndmIdi0cy <42308256+rndmIdi0cy@users.noreply.github.com>
2020-05-06 09:17:28 -0400

    added eventlogs, recent files list, LNK files
    moved IR output path to global var and actually remembered to add output custom output folder this time

--- menagerie.ps1
+++ menagerie.ps1
@@ -1,22 +1,20 @@
 Param (
     [Parameter(Mandatory = $false)]
-    [string] $module
+    [string] $module,
+
+    [Parameter(Mandatory = $false)]
+    [string] $folder
 )
 
+[string] $date = Get-Date -Format yyyyMMddHHmmss
 [string] $ComputerName = $Env:COMPUTERNAME
-[string] $irPath = "C:\Windows\Temp"
-
-if (!(Test-Path $irPath)) {
-    mkdir -Path $irPath -Force
-}
-
 
 function usage {
     Write-Output "
     Usage:
       -module all           : run all modules
       -module <name>        : run specific module
-      -folder <path>        : output folder [Default: C:\Windows\Temp]
+      -folder <path>        : output folder [Default: C:\Windows\Temp\IR]
       -module help          : display usage
 
     Modules:
@@ -29,6 +27,9 @@ function usage {
       PEFiles               : Get list of PE files and hashes in user writeable locations
       OfficeFiles           : Get list of office docs and hashes in user writeable locations
       ScriptFiles           : Get list of scripts and hashes in user writeable locations
+      EventLogs             : Gather Event Logs
+      RecentFiles           : Get history of recent files
+      LNKFiles              : Get LNK files on desktop and recent files list
 
     Examples:
       runscript -CloudFile='Menagerie' -CommandLine='-module all'
@@ -38,7 +39,7 @@ function usage {
 
 function Get-AutoRuns {
     Write-Output "[+] Gathering Windows AutoRuns ..."
-    $outputFile = Join-Path $irPath "\${ComputerName}_AutoRuns.csv"
+    $outputFile = Join-Path $Global:irPath "\${ComputerName}_AutoRuns.csv"
     $results = Get-CimInstance -Class Win32_StartupCommand | Select-Object Name, Caption, Description, Command, Location, User
     $results | Export-Csv -NoTypeInformation -Path $outputFile
     Write-Output "[ done ]"
@@ -47,7 +48,7 @@ function Get-AutoRuns {
 
 function Get-Services {
     Write-Output "[+] Gathering Windows Services ..."
-    $outputFile = Join-Path $irPath "\${ComputerName}_Services.csv"
+    $outputFile = Join-Path $Global:irPath "\${ComputerName}_Services.csv"
     $results = Get-CimInstance -Class Win32_Service -Filter "Caption LIKE '%'" | Select-Object Caption, Description, DisplayName, Name, PathName, ProcessId, StartMode, State, Status
     $results | Export-Csv -NoTypeInformation -Path $outputFile
     Write-Output "[ done ]"
@@ -56,7 +57,7 @@ function Get-Services {
 
 function Get-InstalledSoftware {
     Write-Output "[+] Gathering Installed Software ..."
-    $outputFile = Join-Path $irPath "\${ComputerName}_InstalledSoftware.csv"
+    $outputFile = Join-Path $Global:irPath "\${ComputerName}_InstalledSoftware.csv"
     $regPaths = @("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
         "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\*")
 
@@ -72,7 +73,7 @@ function Get-InstalledSoftware {
 
 function Get-DNSCache {
     Write-Output "[+] Gathering DNS Client Cache ..."
-    $outputFile = Join-Path $irPath "\${ComputerName}_DNSClientCache.csv"
+    $outputFile = Join-Path $Global:irPath "\${ComputerName}_DNSClientCache.csv"
     Get-DnsClientCache | Select-Object TTL, Data, DataLength, Entry, Name, TimeToLive, Type | Export-Csv -NoTypeInformation -Path $outputFile
     Write-Output "[ done ]"
 }
@@ -80,7 +81,7 @@ function Get-DNSCache {
 
 function Get-RunningProcesses {
     Write-Output "[+] Gathering Running Processes ..."
-    $outputFile = Join-Path $irPath "\${ComputerName}_Processes.csv"
+    $outputFile = Join-Path $Global:irPath "\${ComputerName}_Processes.csv"
 
     $procs = Get-Process -IncludeUserName
 
@@ -98,8 +99,8 @@ function Get-RunningProcesses {
 
 function Get-Prefetch {
     Write-Output "[+] Gathering Prefetch Cache ..."
-    $outputFile = Join-Path $irPath "\${ComputerName}_Prefetch.csv"
-    $results = Get-ChildItem -Path "C:\Windows\Prefetch\" | Select-Object Name, FullName, CreationTimeUtc, LastAccessTimeUtc, LastWriteTimeUtc
+    $outputFile = Join-Path $Global:irPath "\${ComputerName}_Prefetch.csv"
+    $results = Get-ChildItem -Path "C:\Windows\Prefetch\" -Filter *.pf -ErrorAction SilentlyContinue | Select-Object Name, FullName, CreationTimeUtc, LastAccessTimeUtc, LastWriteTimeUtc
 
     $results | Export-Csv -NoTypeInformation -Path $outputFile
     Write-Output "[ done ]"
@@ -108,7 +109,7 @@ function Get-Prefetch {
 
 function Get-PEFiles {
     Write-Output "[+] Gathering list of PE files in TEMP locations ..."
-    $outputFile = Join-Path $irPath "\${ComputerName}_PEFiles.csv"
+    $outputFile = Join-Path $Global:irPath "\${ComputerName}_PEFiles.csv"
 
     $PEExtPattern = ".exe|.dll|.sys"
     $filePaths = @(
@@ -124,16 +125,16 @@ function Get-PEFiles {
     try {
         Foreach ($path in $filePaths) {
             Get-ChildItem -Force -Recurse -Path $path -Attributes !System, !ReparsePoint -ErrorAction SilentlyContinue |
-            Where-Object { $_.Extension -match $PEExtPattern } |
-            ForEach-Object {
-                $filePath = $_.FullName
-                $hash = (Get-FileHash -Path $filePath -Algorithm SHA256).Hash
+                Where-Object { $_.Extension -match $PEExtPattern } |
+                ForEach-Object {
+                    $filePath = $_.FullName
+                    $hash = (Get-FileHash -Path $filePath -Algorithm SHA256).Hash
 
-                $peFiles += New-Object PSObject -Property @{
-                    Hash     = $hash
-                    FilePath = $filePath
+                    $peFiles += New-Object PSObject -Property @{
+                        Hash     = $hash
+                        FilePath = $filePath
+                    }
                 }
-            }
         }
     }
     catch { }
@@ -145,7 +146,7 @@ function Get-PEFiles {
 
 function Get-OfficeTextFiles {
     Write-Output "[+] Gathering list of Office files in TEMP locations ..."
-    $outputFile = Join-Path $irPath "\${ComputerName}_OfficeFiles.csv"
+    $outputFile = Join-Path $Global:irPath "\${ComputerName}_OfficeFiles.csv"
 
     $PEExtPattern = ".docx|.docm|.xlsx|.xlsm|.pptx|.pptm"
     $filePaths = @(
@@ -161,16 +162,16 @@ function Get-OfficeTextFiles {
     try {
         Foreach ($path in $filePaths) {
             Get-ChildItem -Force -Recurse -Path $path -Attributes !System, !ReparsePoint -ErrorAction SilentlyContinue |
-            Where-Object { $_.Extension -match $PEExtPattern } |
-            ForEach-Object {
-                $filePath = $_.FullName
-                $hash = (Get-FileHash -Path $filePath -Algorithm SHA256).Hash
+                Where-Object { $_.Extension -match $PEExtPattern } |
+                ForEach-Object {
+                    $filePath = $_.FullName
+                    $hash = (Get-FileHash -Path $filePath -Algorithm SHA256).Hash
 
-                $peFiles += New-Object PSObject -Property @{
-                    Hash     = $hash
-                    FilePath = $filePath
+                    $peFiles += New-Object PSObject -Property @{
+                        Hash     = $hash
+                        FilePath = $filePath
+                    }
                 }
-            }
         }
     }
     catch { }
@@ -182,7 +183,7 @@ function Get-OfficeTextFiles {
 
 function Get-ScriptFiles {
     Write-Output "[+] Gathering list of Script files in TEMP locations ..."
-    $outputFile = Join-Path $irPath "\${ComputerName}_ScriptFiles.csv"
+    $outputFile = Join-Path $Global:irPath "\${ComputerName}_ScriptFiles.csv"
 
     $PEExtPattern = ".bat|.vbs|.cmd|.js|.com|.ps1|.psm|.psm1|.psd|.txt"
     $filePaths = @(
@@ -198,22 +199,66 @@ function Get-ScriptFiles {
     try {
         Foreach ($path in $filePaths) {
             Get-ChildItem -Force -Recurse -Path $path -Attributes !System, !ReparsePoint -ErrorAction SilentlyContinue |
-            Where-Object { $_.Extension -match $PEExtPattern } |
-            ForEach-Object {
-                $filePath = $_.FullName
-                $hash = (Get-FileHash -Path $filePath -Algorithm SHA256).Hash
+                Where-Object { $_.Extension -match $PEExtPattern } |
+                ForEach-Object {
+                    $filePath = $_.FullName
+                    $hash = (Get-FileHash -Path $filePath -Algorithm SHA256).Hash
 
-                $peFiles += New-Object PSObject -Property @{
-                    Hash     = $hash
-                    FilePath = $filePath
+                    $peFiles += New-Object PSObject -Property @{
+                        Hash     = $hash
+                        FilePath = $filePath
+                    }
                 }
-            }
         }
     }
     catch { }
 
     $peFiles | Export-Csv -NoTypeInformation -Path $outputFile
     Write-Output "[ done ]"
+}
+
+function Get-EventLogs {
+    Write-Output "[+] Gathering Event Logs ..."
+    $ir_evtx_path = Join-Path $Global:irPath "\${ComputerName}_evtx"
+    New-Item -Path $ir_evtx_path -Type Directory | Out-Null
+    $evtx_files = Get-ChildItem -Path "C:\Windows\system32\winevt\logs\" -Filter *.evtx -ErrorAction SilentlyContinue
+
+    foreach ($file in $evtx_files) {
+        Copy-Item -Path $file.FullName -Destination $ir_evtx_path -ErrorAction SilentlyContinue
+    }
+
+    Write-Output "[ done ]"
+
+}
+
+function Get-RecentFiles {
+    Write-Output "[+] Gathering Recent File Cache ..."
+    $ir_recentfiles_path = Join-Path $Global:irPath "\${ComputerName}_RecentFiles"
+    New-Item -Path $ir_recentfiles_path -Type Directory | Out-Null
+
+    $recentfiles_path = "C:\Windows\AppCompat\Programs\RecentFileCache.bcf"
+
+    if (Test-Path $recentfiles_path) {
+        Copy-Item -Path $recentfiles_path -Destination $ir_recentfiles_path
+    }
+
+    Write-Output "[ done ]"
+}
+
+function Get-LnkFiles {
+    Write-Output "[+] Gathering LNK files ..."
+    $ir_lnkfiles_path = Join-Path $Global:irPath "\${ComputerName}_LnkFiles"
+    New-Item -Path $ir_lnkfiles_path -Type Directory | Out-Null
+
+    $lnkfiles_path = @("${env:LOCALAPPDATA}\Microsoft\Windows\Recent\", "${env:LOCALAPPDATA}\Microsoft\Office\Recent\", "C:\Users\*\Desktop\")
+
+    foreach ($path in $lnkfiles_path) {
+        $lnk_files = Get-ChildItem -Path $path -Filter *.lnk -Recurse -ErrorAction SilentlyContinue
+
+        foreach ($file in $lnk_files) {
+            Copy-Item -Path $file.FullName -Destination $ir_lnkfiles_path -ErrorAction SilentlyContinue
+        }
+    }
 }
 
 
@@ -228,12 +273,26 @@ function Invoke-AllIRModules {
     Get-PEFiles
     Get-OfficeTextFiles
     Get-ScriptFiles
+    Get-EventLogs
+    Get-RecentFiles
+    Get-LnkFiles
 }
 
 if ($module) {
+    if ($folder) {
+        if (-Not (Test-Path $folder)) {
+            New-Item -Path $folder -Type Directory -Force | Out-Null
+        }
+
+        $Global:irPath = $folder
+    }
+    else {
+        $Global:irPath = "C:\Windows\Temp\IR"
+    }
+
     switch ($module.ToLower()) {
         all { Invoke-AllIRModules }
-        autoruns { Get-AutoRuns($irPath) }
+        autoruns { Get-AutoRuns }
         installedsoftware { Get-InstalledSoftware }
         dnscache { Get-DNSCache }
         runningprocesses { Get-RunningProcesses }
@@ -241,6 +300,9 @@ if ($module) {
         pefiles { Get-PEFiles }
         officefiles { Get-OfficeFiles }
         scriptfiles { Get-ScriptFiles }
+        eventlogs { Get-EventLogs }
+        recentfiles { Get-RecentFiles }
+        lnkfiles { Get-LnkFiles }
         help { usage }
         default { usage }
     }
